e9bc3f697fe619df78d34e528ba7df8eadcd16ef
LeeSH <tkdgurdhkd99@naver.com>
2025-02-17 09:50:30 +0900

    v0.3 day06 is_even_bit

--- day06.py
+++ day06.py
@@ -1,7 +1,7 @@
-def is_even(num):
-    if num % 2 == 0:
-        return True
-    return False
+# def is_even(num):
+#     if num % 2 == 0:
+#         return True
+#     return False
 
 a=10    #0000 1010
 b=11    #0000 1011  :   & => 0000 1010 , | => 0000 1011 , ^(NOR) => 0000 0001
@@ -12,3 +12,12 @@ print(a ^ b)
 
 # n = int(input())
 # print(is_even(n))
+
+def is_even_bit(num):
+    # if num & 1 == 1:
+    #     return False
+    # return True
+    return not num & 1
+
+n = int(input('num : '))
+print(is_even_bit(n))
