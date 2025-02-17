5b84188d8f861e8290488371900c9ed956f0c45b
LeeSH <tkdgurdhkd99@naver.com>
2025-02-17 16:30:10 +0900

    v1.2 day06 File io

--- day06.py
+++ day06.py
@@ -1,11 +1,10 @@
-import numpy as np
-
-narr = np.array([1,3.1,2,9])    #numpy는 C에 사용. 원소를 높은 type으로 다 변환시킴 float64 = double
-#타입이 같아야 다루기 편하다
-print(type(narr))
-print(type(narr), type(narr[2]),type(narr[3]))
-print(narr[0],narr[1],narr[2],narr[3])
-
-array = [9,-11,8,7]
-print(array[0],array[1],array[2],array[3])
-print(id(array[0]),id(array[1]),id(array[2]),id(array[3]))
+try :
+    fn = input("Enter the File Name : ")
+    fp = open(fn,'r')
+    readme_list = fp.readlines()
+    rls = readme_list[0].split('_')
+    print(readme_list)
+    print(rls)
+    fp.close()
+except FileNotFoundError as err:
+    print(f'{fn} does not exist. {err}')
