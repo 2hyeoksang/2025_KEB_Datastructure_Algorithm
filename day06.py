714545bf256f523c69151010073f5f8021de180b
LeeSH <tkdgurdhkd99@naver.com>
2025-02-17 09:43:07 +0900

    v0.2 day06 bit operation

--- day06.py
+++ day06.py
@@ -2,3 +2,13 @@ def is_even(num):
     if num % 2 == 0:
         return True
     return False
+
+a=10    #0000 1010
+b=11    #0000 1011  :   & => 0000 1010 , | => 0000 1011 , ^(NOR) => 0000 0001
+# bit operation
+print(a & b)
+print(a | b)
+print(a ^ b)
+
+# n = int(input())
+# print(is_even(n))
