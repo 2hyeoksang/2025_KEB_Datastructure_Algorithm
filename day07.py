0866b5db3c89654ba3300e6b701ec2e60b010c7c
LeeSH <tkdgurdhkd99@naver.com>
2025-02-18 16:06:37 +0900

    v2.3 day07 Queue with two stacks

--- day07.py
+++ day07.py
@@ -1,42 +1,22 @@
-import random
-
-class Node:
-    def __init__(self, data, next= None):
-        self.data = data
-        self.next = next
-
 class Queue:
     def __init__(self):
-        self.front = None
-        self.rear = None
-        self._size = 0
+        self.s1 = []
+        self.s2 = []
 
 
     def enqueue(self, data):
-        self._size += 1
-        node = Node(data)
-        if self.rear is None:
-            self.front = node
-            self.rear = node
-        else:
-            self.rear.next = node
-            self.rear = node
+        while len(self.s1) != 0:
+            self.s2.append(self.s1.pop())
+        self.s1.append(data)
+        while len(self.s2) != 0:
+            self.s1.append(self.s2.pop())
 
 
     def dequeue(self):
-        if self.front is None:
-            raise IndexError('dequeue from empty queue')
-        self._size -= 1
-        temp = self.front   #Back-Up
-        self.front = self.front.next    #Update
-        if self.front is None :
-            self.rear = None
+        if len(self.s1) == 0:
+            raise Exception('Cannnot Pop From Empty Queue')
+        return self.s1.pop()
 
-        return temp.data
-
-
-    def size(self):
-        return self._size
 
 
 if __name__ == "__main__":
@@ -44,6 +24,4 @@ if __name__ == "__main__":
     q.enqueue(7)
     q.enqueue(-11)
     q.enqueue(8)
-    print(q.size())
     print(q.dequeue())
-    print(q.size())
