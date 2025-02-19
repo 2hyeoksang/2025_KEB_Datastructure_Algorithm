c37ffbe3a317e2f3d037958f1477ce931e123f68
LeeSH <tkdgurdhkd99@naver.com>
2025-02-19 13:43:35 +0900

    v3.1 day08 find_group

--- day08.py
+++ day08.py
@@ -1,58 +1,55 @@
-def pre_order(node):
-    if node is None:
-        return
-    print(node.data,end = '-')
-    pre_order(node.left)
-    pre_order(node.right)
-
-
-def post_order(node):
-    if node is None:
-        return
-    post_order(node.left)
-    post_order(node.right)
-    print(node.data, end = '-')
-
-
-def in_order(node):
-    if node is None:
-        return
-    in_order(node.left)
-    print(node.data,end = '-')
-    in_order(node.right)
-
-
 class Treenode():
     def __init__(self):
         self.left = None
         self.right = None
         self.data = None
 
-node1 = Treenode()
-node1.data = '화사'
 
-node2 = Treenode()
-node2.data = '솔라'
-node1.left = node2
 
-node3 = Treenode()
-node3.data = '문별'
-node1.right = node3
+if __name__ == "__main__":
+    root = None
+    groups = ['블랙핑크','레드벨벳','마마무','에이핑크','걸스데이','트와이스']
 
-node4 = Treenode()
-node4.data = '휘인'
-node2.left = node4
+    node = Treenode()
+    node.data = groups[0]
+    root = node
 
-node5 = Treenode()
-node5.data = '쯔위'
-node2.right = node5
+    for group in groups[1:]:
+        node = Treenode()
+        node.data = group
+        current = root
 
-node6 = Treenode()
-node6.data = '선미'
-node3.left = node6
+        while True:
+            if group < current.data :
+                if current.left is None :
+                    current.left = node
+                    break
+                current = current.left
 
-post_order(node1)
-print()
-pre_order(node1)
-print()
-in_order(node1)
+            else:
+                if current.right is None:
+                    current.right = node
+                    break
+                current = current.right
+
+
+print("Success")
+
+find_group = input("name: ")
+current =root
+while True :
+    if find_group == current.data :
+        print(f'{find_group}을 찾았습니다.')
+        break
+    elif find_group < current.data:
+        if current.left is None:
+            print(f'{find_group}이 존재하지 않습니다.')
+            break
+        current = current.left
+    else:
+        if current.right is None:
+            print(f'{find_group}이 존재하지 않습니다.')
+            break
+        current = current.right
+
+
