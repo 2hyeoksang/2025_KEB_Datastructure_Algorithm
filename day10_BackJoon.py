04f9a745411b7186ba3244c065586680d6249c6c
LeeSH <tkdgurdhkd99@naver.com>
2025-02-21 17:33:06 +0900

    v4.2 day10 BaekJoon 1158

--- day10_BackJoon.py
+++ day10_BackJoon.py
@@ -1,58 +1,15 @@
-arr1 = []
 
-def push(num):
-    arr1.append(num)
+N, k = tuple(map(int,input().split()))
+arr = [i for i in range(1, N+1)]
+start = 0
+dead_list=[]
+while arr:
+    kill = (start + (k-1)) % len(arr)
+    dead = arr.pop(kill)
+    dead_list.append(dead)
+    start = kill
 
-
-def size(arr1):
-    return len(arr1)
-
-
-def pop(arr1):
-    if size(arr1):
-        return arr1.pop(0)
-    else:
-        return -1
-
-
-def is_empty(arr1):
-    if size(arr1):
-        return 0
-    else:
-        return 1
-
-
-def front(arr1):
-    if size(arr1):
-        return arr1[0]
-    else:
-        return -1
-
-
-def back(arr1):
-    if size(arr1):
-        return arr1[-1]
-    else:
-        return -1
-
-
-N = int(input())
-command = [input() for _ in range(N)]
-for cmd in command :
-    if ' ' in cmd:
-        f, num = cmd.split()
-        num = int(num)
-        if f == "push":
-            push(num)
-
-    else:
-        if cmd == "front":
-            print(front(arr1))
-        elif cmd == "back":
-            print(back(arr1))
-        elif cmd == "pop":
-            print(pop(arr1))
-        elif cmd == "size":
-            print(size(arr1))
-        elif cmd == "empty":
-            print(is_empty(arr1))
+print('<', end='')
+for ele in dead_list[:-1]:
+    print(ele, end =', ')
+print(f'{dead_list[-1]}>')
