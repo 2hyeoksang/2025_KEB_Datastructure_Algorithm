9929f783ea6e0ef02659b4d72d7e55b66a424616
LeeSH <tkdgurdhkd99@naver.com>
2025-02-21 11:12:07 +0900

    v3.9 day10 bubble sort no_swap

--- day10.py
+++ day10.py
@@ -1,10 +1,15 @@
 def bubble_sort(arr):
     l_length = len(arr) - 1
     for i in range(l_length):
+        t1 = True
         for j in range(l_length):
             if arr[j] > arr[j+1]:
                 arr[j] , arr[j+1] = arr[j+1] , arr[j]
-
+                t1 = False
+                print(j,end= ' ')
+        if t1:
+            return arr
     return arr
 
 print(bubble_sort([8, -11, 9, 1]))
+print(bubble_sort([1,2,3,4,5,6,7]))
