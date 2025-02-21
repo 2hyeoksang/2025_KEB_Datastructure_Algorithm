cb3a940c6b748008eda405f31becc718052d6e17
LeeSH <tkdgurdhkd99@naver.com>
2025-02-21 13:17:22 +0900

    v4.0 day10 time comparison

--- day10.py
+++ day10.py
@@ -1,3 +1,17 @@
+import time
+import random
+
+def time_decorator(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f'실행시간 : {e-s}')
+        return r
+    return wrapper
+
+
+@time_decorator
 def bubble_sort(arr):
     l_length = len(arr) - 1
     for i in range(l_length):
@@ -6,10 +20,24 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j] , arr[j+1] = arr[j+1] , arr[j]
                 t1 = False
-                print(j,end= ' ')
+                # print(j,end= ' ')
         if t1:
             return arr
     return arr
 
-print(bubble_sort([8, -11, 9, 1]))
-print(bubble_sort([1,2,3,4,5,6,7]))
+@time_decorator
+def insert_sort(arr):
+    for i in range(1, len(arr)):
+        val = arr[i]
+        while i > 0 and arr[i-1] > val:  # i > 0 조건이 없으면 아마 IndexError 날듯
+            arr[i] = arr[i-1]
+            i -= 1
+        arr[i] = val
+
+    return arr
+
+arr = [random.randint(1,100000) for _ in range(10000)]
+new_arr = arr.copy()
+
+print(bubble_sort(arr))
+print(insert_sort(new_arr))
